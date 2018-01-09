4ddff62c9b1703718d8c6e1b4b9b5c37374b7e26
whangwang <whangwang0430@gmail.com>
2018-01-10 04:17:15 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ var user_data = [
     "notify": [
       {
         "type": "city",
-        "cid": "1"
+        "cid": "1",
+        "string": "台北市"
       },
       {
         "type": "reg",
         "cid": "3",
-        "sid": "21"
+        "sid": "21",
+        "string": "台北市文山區"
       }
     ]
   }
@@ -23,14 +25,87 @@ module.exports = function(app) {
     });
 
     app.get('/get_data/:user_id', function(req, res) {
+        var d = [];
+        for(var i = 0; i < user_data.length; i++){
+          if(String(user_data[i].id)==String(req.params.user_id)){
+            d.push(user_data[i]);
+          }
+        }
         var rtn = {
           "msg": "success",
           "id": req.params.user_id,
-          "data": user_data
+          "data": d
         }
         res.json({
           rtn: rtn
         });
+    });
+
+    app.get('/demo_data', function(req, res){
+      var result_591 = [{
+          "title": "全新裝潢頂加住家",
+          "image_url": "https://hp1.591.com.tw/house/active/2016/11/01/147800112645349902_210x158.crop.jpg",
+          "layout": "2房1廳2衛1陽台",
+          "web_url": "https://rent.591.com.tw/rent-detail-5945091.html",
+          "date":  "2018-01-105"
+        }]
+        for( var i = 0; i < 1; i++){
+          element_arr.push({
+            "title":result_591[i].title,
+            "image_url":result_591[i].image_url,
+            "subtitle":result_591[i].layout,
+            "default_action": {
+              "type": "web_url",
+              "url": result_591[i].web_url
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":result_591[i].web_url,
+                "title":"開啟"
+              },
+              {
+                "type": "element_share",
+                "share_contents": {
+                 "attachment": {
+                   "type": "template",
+                   "payload": {
+                      "template_type":"generic",
+                      "elements": [
+                        {
+                          "title":result_591[i].title,
+                          "image_url":result_591[i].image_url,
+                          "subtitle":result_591[i].layout,
+                          "default_action": {
+                            "type": "web_url",
+                            "url": result_591[i].web_url
+                          },"buttons":[
+                           {
+                            "type":"web_url",
+                            "url":result_591[i].web_url,
+                            "title":"View Detail"
+                           }
+                          ]
+                        }
+                      ]
+                    }
+                  }
+                 }
+              }
+            ]
+          });
+        }
+        console.log(element_arr);
+        response = {
+              "attachment": {
+              "type": "template",
+              "payload": {
+                 "template_type":"generic",
+                 "elements": element_arr
+               }
+             }
+         }
+         res.json(response);
     });
 
     app.post('/set_data', function(req, res) {
@@ -47,7 +122,8 @@ module.exports = function(app) {
             if(String(user_data[i].id)==String(id)){
               user_data[i].notify.push({
                 type: type,
-                cid: cid
+                cid: cid,
+                string: string
               });
               find++;
             }
@@ -57,7 +133,8 @@ module.exports = function(app) {
               id: String(id),
               notify: [{
                 type: type,
-                cid: cid
+                cid: cid,
+                string: string
               }]
             });
           }
@@ -78,7 +155,8 @@ module.exports = function(app) {
               user_data[i].notify.push({
                 type: type,
                 cid: cid,
-                sid: sid
+                sid: sid,
+                string: string
               });
               find++;
             }
@@ -89,7 +167,8 @@ module.exports = function(app) {
               notify: [{
                 type: type,
                 cid: cid,
-                sid: sid
+                sid: sid,
+                string: string
               }]
             });
           }
